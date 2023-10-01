19a3bfe41a10349e846857320b5b9fb267a99889
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2023-10-01 14:13:19 +0600

    dice remove at the starting point

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict';
 
-const score0El = document.getElementById('score--1')
+const score0El = document.getElementById('score--0')
+const score1El = document.getElementById('score--1')
+const diceEl = document.querySelector('.hidden')
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden')

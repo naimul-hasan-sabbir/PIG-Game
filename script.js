c5592c04f3d1f08016f432e8e79cff50c4ab0492
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2023-10-02 15:48:29 +0600

    button works

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const score0El = document.getElementById('score--0')
 const score1El = document.getElementById('score--1')
 const diceEl = document.querySelector('.hidden')
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 
 score0El.textContent = 0;
 score1El.textContent = 0;

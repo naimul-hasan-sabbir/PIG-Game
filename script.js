c433684ca8d5c370f5e2d4afebd352b981fd03a8
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2023-10-06 14:02:23 +0600

    current score updated

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const score0El = document.getElementById('score--0')
 const score1El = document.getElementById('score--1')
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
 const diceEl = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
@@ -24,6 +26,7 @@ btnRoll.addEventListener('click', function(){
 
     if(dice !== 1){
         currentScore += dice;
+        current0El.textContent = currentScore;
     }else{
 
     }
